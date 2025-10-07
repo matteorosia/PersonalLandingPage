a288fca66fce9013abf323041bb8f8f854bad415
Matteo <20034444@studenti.uniupo.it>
2025-10-07 23:07:35 +0200

    Alcuni piccoli fix grafici

--- my-project/src/components/CodeSectionTab.jsx
+++ my-project/src/components/CodeSectionTab.jsx
@@ -1,8 +1,8 @@
 export default function CodeSectionTab({ file }) {
   return (
-    <div className="bg-neutral-800 sm:w-[300px] max-sm:w-[480px] h-[40px] 
+    <div className="bg-neutral-800 sm:w-[300px] max-sm:w-[500px] h-[40px] 
         border-r border-t border-t-blue-400 border-r-neutral-600 flex 
-        justify-center items-center relative font-mono max-sm:text-[15px] sm:text-[13px] text-yellow-300 z-50">
+        justify-center items-center relative font-mono max-sm:text-[15px] sm:text-[11px] text-yellow-300 z-50">
         {file}
          <div className="absolute right-2 rounded-full bg-white w-[8px] h-[8px]"></div>
     </div>
