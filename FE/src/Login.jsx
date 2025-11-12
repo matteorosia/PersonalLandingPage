dbfeb4203b114addfccf814688768bd61a92245e
Matteo <20034444@studenti.uniupo.it>
2025-11-12 21:32:44 +0100

    Tampone login

--- FE/src/Login.jsx
+++ FE/src/Login.jsx
@@ -1,20 +1,44 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Login(){
 
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [message, setMessage] = useState("")
+    const navigate = useNavigate()
+
     function handleLoginClick(e){
-        console.log(e.target.value);
+        e.preventDefault()
+        if (username == "Admin" && password == "PippoPluto123"){
+            navigate("/panel");
+        }
+        else{
+            setMessage("Password errata");
+        }
     }
 
-    return <div className="flex flex-col w-full h-screen bg-neutral-700 justify-center items-center">
-       <form id="form" className="flex flex-col gap-4 p-4 w-1/2">
-            <div className="flex justify-center items-center text-2xl text-neutral-300">
+    function handleUser(e){
+        setUsername(e.target.value);
+    }
+
+    function handlePassword(e){
+        setPassword(e.target.value);
+    }
+
+    return <div className="flex flex-col w-full h-screen bg-cyan-700 justify-center items-center font-mono">
+       <form id="form" className="flex flex-col gap-4 p-4 w-2/5 bg-neutral-600 rounded-[10px] shadow-lg">
+            <div className="flex justify-center items-center text-[40px] text-neutral-300 m-[20px]">
                 LOGIN
             </div>
-            <input type="text" className="outline-0 bg-neutral-300 p-4" placeholder="Username"></input> 
-            <input type="password" className="utline-0 bg-neutral-300 p-4"></input> 
+            <div className="flex justify-center items-center flex-col gap-4">
+                <input type="text" className="w-7/10 outline-0 bg-neutral-300 p-4" placeholder="Username" onChange = {handleUser}></input> 
+                <input type="password" className="w-7/10 utline-0 bg-neutral-300 p-4" onChange={handlePassword}></input> 
+            </div>
             <div className="flex flex-col gap-4 justify-center items-center">
                 <button className="bg-neutral-300 p-4 w-[100px]" onClick={handleLoginClick}>Login</button>
+                <p className="text-red-400 uppercase">{message}</p>
                 <Link to="/">Go back</Link>
             </div>
        </form>
